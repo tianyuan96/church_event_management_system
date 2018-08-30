14caf354408ee2b46d2a6877055a54f63fb20243
Jingcheng Li <jingcheng.li@student.unsw.edu.au>
2018-08-30 16:42:29 +1000

    add settings for emails

--- church_booking_system/settings.py
+++ church_booking_system/settings.py
@@ -28,6 +28,14 @@ DEBUG = True
 ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
+# Email server settings
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'jingchengtest@gmail.com'
+EMAIL_HOST_PASSWORD = 'LJCtest1107'
+EMAIL_PORT = 587
+
+
 # Application definition
 
 INSTALLED_APPS = [
