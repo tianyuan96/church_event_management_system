329a036f741947b65837d38c137f1c4716a94045
Jingcheng Li <jingcheng.li@student.unsw.edu.au>
2018-08-31 17:09:35 +1000

    added CONFIRMATIONS TABLE

--- apps/user_accounts/models.py
+++ apps/user_accounts/models.py
@@ -1,3 +1,8 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+class Confirmations(models.Model):
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    confirmation_code = models.CharField(max_length=255)
+    has_confirmed = models.BooleanField(default=False)
