001faa319bd66e6c81349c7859fe73d8a6f18768
Jingcheng Li <jingcheng.li@student.unsw.edu.au>
2018-08-31 17:09:55 +1000

    added urls for email  confirmation

--- apps/user_accounts/urls.py
+++ apps/user_accounts/urls.py
@@ -1,5 +1,6 @@
 from . import views
 from django.urls import path, include
+from django.conf.urls import url
 
 urlpatterns = [
 
@@ -8,5 +9,9 @@ urlpatterns = [
     path('login/', views.LoginUserView.as_view(), name="user_login"),
     path('reset/', views.ResetUserView.as_view(), name="password_reset"),
     path('logout/', views.LogoutUserView.as_view(), name="user_logout"),
+    path('successfully_registered/', views.NeedActivateView.as_view(), name="successfully_registered"),
+    path('back_homepage/', views.BackHomepageView.as_view(), name="back_home_page"),
+    url(r'^user_confirm/(?P<confirmation_code>.*)/$', views.UserConfirmView.as_view(), name="user_confirm"),
+    path('successfully_confirmed/', views.HasActivatedView.as_view(), name="successfully_confirmed"),
 
 ]
