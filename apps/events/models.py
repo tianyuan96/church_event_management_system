7df111a20843fbb1fdd9b6b8bde88ae660b68b9b
Jingcheng Li <jingcheng.li@student.unsw.edu.au>
2018-09-02 12:41:58 +1000

    add modify event

--- apps/events/models.py
+++ apps/events/models.py
@@ -8,6 +8,7 @@ class Event(models.Model):
     name = models.CharField(max_length=100)
     date = models.DateField()
     location = models.CharField(max_length=100)
+    description = models.CharField(max_length=512)
 
 
 class InvolvedEvent(models.Model):
--- apps/events/views.py
+++ apps/events/views.py
@@ -1,11 +1,29 @@
 from django.shortcuts import render
 from django.views import generic
 from .forms import EventCreationForm
+from . import models
 from django.shortcuts import render
+from django.urls import reverse_lazy
 
 
 class EventView(generic.FormView):
 
     template_name = "main/create_event.html"
     form_class = EventCreationForm
+
+
+
+class ModifyEventView(generic.View):
+
+    successful_url = reverse_lazy('successfully_modified')
+
+    def get(self, request, *args, **kwargs):
+        event = models.Event.objects.get(id=id)
+
+        event.name = request.GET.get('name')
+        event.date = request.GET.get('date')
+        event.location = request.GET.get('venue')
+        event.description = request.GET.get('description')
+        event.save()
+        return render(request, self.template_name, {'user': request.user, 'title': request.user.email})
 
