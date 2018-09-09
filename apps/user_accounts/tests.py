7fe2bf5a1bdc8a99b0e08ac19c8422d79a5a1f0c
Michelle Leman <z5076202@ad.unsw.edu.au>
2018-09-09 19:09:02 +1000

    tests written

--- apps/user_accounts/tests.py
+++ apps/user_accounts/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
 
-# Create your tests here.
+class UserTest(TestCase):
+
+    def setUp(self):
+        User.objects.create(email='random@gmail.com', password='auise1234', username='random@gmail.com')
+        #User.objects.create(email='random1@gmail.com', password='auise1234', username='random@gmail.com')
+
+
+    def testWorkingUser(self):
+        user1 = User.objects.get(username='random@gmail.com')
+        # Test that the email becomes our username
+        self.assertEqual(user1.email, user1.username)
+        # Test that the password is greater than 8 characters
+        self.assertTrue(len(user1.password)>8)
+        #self.assertIs(user1.password, int(user1.password))
+        self.assertIn('@', user1.email)
+        # Testing that the password is all numeric
+        try:
+            int(user1.password)
+            raise AssertionError
+        except ValueError:
+        # If it is not we can continue
+            pass
