6a7a226cc412bc292476838014f98be94098aac9
SamTheNormalOne <samscheding@gmail.com>
2018-10-15 23:24:08 +1100

    added python anywhere to the allowed hosts

--- church_booking_system/settings.py
+++ church_booking_system/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '!k&+g1(d!gozidu&c*sh6wuje3ngr-77o7ubk(@bi^lg@(jn#g'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'samscheding1.pythonanywhere.com/']
 
 
 # Email server settings
