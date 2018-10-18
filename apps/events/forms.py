34ca91f7ed41370d1802b3531c9835c4df35b3b0
Michelle Leman <z5076202@ad.unsw.edu.au>
2018-10-18 17:17:02 +1100

    events cannot be made in the past

--- apps/events/forms.py
+++ apps/events/forms.py
@@ -9,6 +9,12 @@ class EventCreationForm(forms.ModelForm):
     date = forms.DateField(widget=forms.DateInput(attrs={'class': 'form-control',
                                            'type': 'date'}),
                            initial=datetime.date.today)
+    def clean_date(self):
+        date = self.cleaned_data['date']
+        if date < datetime.date.today():
+            raise forms.ValidationError("The date cannot be in the past!")
+        else:
+            return date
 
     class Meta:
         model = Event
